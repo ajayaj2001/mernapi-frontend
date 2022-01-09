e562ce655f3cb0a7d2a3c5c14e56cc4bdaf8755b
Ajay <65935140+ajayaj2001@users.noreply.github.com>
2022-01-09 12:19:19 +0530

    comment added

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,1 +1,1 @@
-export const baseURL = "https://mernapi-backend.herokuapp.com/api"; //api url
+export const baseURL = "https://mernapi-backend.herokuapp.com/api"; //api url  //for local backend server change to http://localhost:4000/api
